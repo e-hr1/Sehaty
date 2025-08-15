340c810b89aec06fcff4b6671a0275b21796f5f2
Al Hussein Mutair <121640437+e-hr1@users.noreply.github.com>
2025-08-15 03:54:10 +0300

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,21 +1,28 @@
 // تهيئة Firebase
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-database.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/12.1.0/firebase-storage.js";
+
+// تكوين Firebase الخاص بك
 const firebaseConfig = {
-    apiKey: "AIzaSyDP1PoXMtol_LKbDTnNW_hLjdEVn9MxslA",
-    authDomain: "arab-24-default-rtdb.firebaseapp.com",
-    databaseURL: "https://arab-24-default-rtdb.firebaseio.com",
-    projectId: "arab-24-default-rtdb",
-    storageBucket: "arab-24-default-rtdb.appspot.com",
-    messagingSenderId: "1051707223171",
-    appId: "1:1051707223171:android:9ef039ef2b1306c3e8ffb6"
+  apiKey: "AIzaSyAz8COaUKi91zqLCiawMOo-I51FgHx8gp8",
+  authDomain: "arab-24.firebaseapp.com",
+  databaseURL: "https://arab-24-default-rtdb.firebaseio.com",
+  projectId: "arab-24",
+  storageBucket: "arab-24.appspot.com",
+  messagingSenderId: "1051707223171",
+  appId: "1:1051707223171:web:0f83c22a4ff9a042e8ffb6",
+  measurementId: "G-B5EVD91MHD"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+// تهيئة Firebase
+const app = initializeApp(firebaseConfig);
 
-// المراجع
-const auth = firebase.auth();
-const database = firebase.database();
-const storage = firebase.storage();
-
+// تصدير الخدمات المطلوبة
+export const auth = getAuth(app);
+export const database = getDatabase(app);
+export const storage = getStorage(app);
+export const googleProvider = new GoogleAuthProvider();
 // مزود المصادقة - Google
 const googleProvider = new firebase.auth.GoogleAuthProvider();
